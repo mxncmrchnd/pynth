bcd505af6c04466873cff0ca638f8d4cbf01bb62
Maxence Marchand <mxnc.marchand@gmail.com>
2026-02-11 15:37:27 +0100

    updated CLI, added dependencies to pyproject.toml

--- README.md
+++ README.md
@@ -13,19 +13,23 @@ by Maxence Marchand, 2026
 ## How to run pynth
 Open a terminal in the `pynth` folder and type : 
 
-``` pip install -e .```
+```pip install -e .```
 
 If you wish to run the GUI version (recommended) : 
 
-``` pynth-gui ``` and play around.
+```pynth-gui``` and play around.
 
 ![GUI screenshot](screenshot.png)
 
 If you wish to use the CLI version : 
 
-``` pynth-cli input.mid output.flac ```
+```pynth-cli input.mid output.flac```
 
-Improvements to the CLI version are being worked on, to allow ADSR envelope modification and effects.
+**IMPORTANT** The CLI version is limited in features. It uses the default envelope, and only one effect can be applied at a time, with default parameters.
+
+Example usage : 
+
+```pynth-cli input.mid output.flac --waveform square --effect chorus```
 
 ## Dependencies
 - `matplotlib`
--- src/pynth/cli.py
+++ src/pynth/cli.py
@@ -10,9 +10,10 @@ def main():
     parser.add_argument("input_midi", type = midi.check_midi_input_path, help = "The MIDI file to be read")
     parser.add_argument("output_flac", type = midi.check_flac_output, help = "The output FLAC")
     parser.add_argument("--waveform", choices = ["saw", "sine", "square", "triangle"], default = "sine", help = "The waveform type")
+    parser.add_argument("--effect", choices = ["chorus", "delay", "reverb"], default = None, help = "The type of effect to be applied")
     args = parser.parse_args()
     ## build the paths
     midi_path = os.path.abspath(args.input_midi)
     output_path = os.path.abspath(args.output_flac)
     # call the synth function
-    midi.midi_to_flac(midi_path, output_path, wf = args.waveform, fx = defaults.DEFAULT_EFFECTS)
+    midi.midi_to_flac(midi_path, output_path, wf = args.waveform, fx = args.effect)
